b77eb946fb35c14e1eba1b2414d0f85ab9e0ce73
PSH <pshoben@yahoo.com>
2020-09-22 10:36:17 -0400

    checkpoint yaml

--- harness/class/fields_harness.main.cpp
+++ harness/class/fields_harness.main.cpp
@@ -143,7 +143,7 @@ void to_yaml3(std::ostream& out, const Test::SimpleType3 * instance)
 
 	Class<Test::SimpleType3>::print_class_yaml(
 	instance, "", 
-	[&out](std::string indent, auto& name, auto& value)
+	[&out](std::string indent, auto& name, auto value)
 	{
 		//out << indent << name << "\n";
 		out << indent << name << value << "\n";
@@ -179,9 +179,10 @@ void from_yaml3(std::istream& in, Test::SimpleType3 * instance)
 		if( ptr_last_space ) {
 			strcpy( value, ptr_last_space+1 );
 		} else {
+			//std::cout << "from_yaml3 setting *value=0\n";
 			*value=0; // empty string
 		}
-		std::cout << "expecting " << "[" << indent << "] ... [ " << name << "] ... got: [" << value << "]\n";
+		//std::cout << "from_yaml3 expecting " << "[" << indent << "] ... [" << name << "] ... tempbuf [" << ptr_last_space << "] got: [" << *value << "] str value addr = (" << value << ")\n";
 	});
 }
 
@@ -226,6 +227,7 @@ void test_read_simpletype3(void)
 {
   printf("in %s:\n", __FUNCTION__);
   Test::SimpleType3 simple{ 456,(long)(1 << 31),'a',0.123, 0.3-0.1, 2 } ;
+
 simple.psub1 = new Test::SimpleType2
 {
 556,(long)(1 << 31),'a',0.123, 0.3-0.1, 
@@ -255,15 +257,19 @@ simple.psubarray[1] = new Test::SimpleType2
    { 1.3-0.1, 0.3-0.2}
 };
 
+  simple.baseint_field = 99;
+  simple.pbaseint_field = new int(199);
+
+
 std::ostringstream string_out;
   to_yaml3(string_out, &simple);
   std::cout << string_out.str() ;
 
   std::istringstream string_in{string_out.str()};
-  Test::SimpleType3 * psimple_copy = new Test::SimpleType3{};
+   Test::SimpleType3 * psimple_copy = new Test::SimpleType3{};
   from_yaml3(string_in, psimple_copy);
 
-  std::cout << "after copying...";
+  std::cout << "after copying...\n";
 
   to_yaml3(std::cout, psimple_copy);
 
